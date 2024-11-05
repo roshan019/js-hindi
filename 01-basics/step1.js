81a1756246f57a23abfc01204a7c477a185aab71
roshan019 <139036238+roshan019@users.noreply.github.com>
2024-11-05 16:57:30 +0000

    study about JavaScript variables (let,const,var) keyword.

--- 01-basics/step1.js
+++ 01-basics/step1.js
@@ -1,1 +1,13 @@
-console.log("roshan budha");
+const accountId = 12345;
+let accountEmail ="roshn@gmail.com";
+var accountPassword = "123456";
+accountCity = "nepal";
+accountCity = "tikapur"
+accountEmail = "budha@gmail.com"
+//accountId = 3 // not allowed to change the const variables
+// console.log(accountId);
+//console.log([accountEmail,accountId,accountPassword,accountCity]);
+// console.log(accountCity);
+console.log(accountEmail);
+
+
