a71706e7160b26b8019439797d97386c5bb25f4f
roshan019 <139036238+roshan019@users.noreply.github.com>
2024-11-06 16:59:40 +0000

    conversion of datatyps

--- 01-basics/03-dataconversion.js
+++ 01-basics/03-dataconversion.js
@@ -29,3 +29,38 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+
+
+// ********************** Operations **************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello";
+let str2 = " roshan";
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+// console.log("1"+ 2 + 2);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3 // this is not right way to write the code 
+num1 = num2 = num3 = 2 + 2; // always avoid to write the code like this. 
+
+let gameCounter = 100;
+++gameCounter;
+// console.log(gameCounter);
+
+
+
+
+
+
