70ee18b2f8c94200a39f9c3805fd8bb368cf9d01
roshan019 <139036238+roshan019@users.noreply.github.com>
2024-11-06 16:12:02 +0000

    Datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+
+// console.log(3
+//     +
+//     3);  // code readability should be high
+
+// console.log("roshan");
+
+let name = "roshan";
+let isLoggedIn = false;
+
+// number => 2 to power 53
+//bigint
+//string =""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof null); //null is a object
+
