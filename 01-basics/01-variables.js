a2213d14c7cdc2cd172685a933bc7a0b33194538
roshan019 <139036238+roshan019@users.noreply.github.com>
2024-11-06 15:41:43 +0000

    learning about datatypes

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,19 @@
+const accountId = 12345;
+let accountEmail ="roshn@gmail.com";
+var accountPassword = "123456";
+
+
+/*
+    prefer not to used var
+    becaused to issues in functional scope and block scope
+*/
+accountCity = "nepal";
+accountCity = "tikapur"
+accountEmail = "budha@gmail.com"
+//accountId = 3 // not allowed to change the const variables
+// console.log(accountId);
+//console.log([accountEmail,accountId,accountPassword,accountCity]);
+// console.log(accountCity);
+console.log(accountEmail);
+
+
