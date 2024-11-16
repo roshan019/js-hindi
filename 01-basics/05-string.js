aee3d43b177bd396023a9b1b8349ca7c9e102e2e
roshan019 <139036238+roshan019@users.noreply.github.com>
2024-11-16 17:30:02 +0000

    learning about the string method

---
+++ 01-basics/05-string.js
@@ -0,0 +1,30 @@
+
+// String in JavaScript 
+ const name ="roshan "
+ const repoCount = 50
+
+//  console.log(name[1]);
+ 
+//  console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+//  console.log(typeof name);
+
+ const gameName = new String('candycrush')
+ 
+//  console.log( typeof gameName);
+ 
+// console.log(gameName.slice(2,6));
+// console.log(gameName);
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);  // find length of the string
+// console.log(gameName.toUpperCase);  // convert the string to uppercase
+// console.log(gameName.charAt(3));  // find the value from the string using a index of the sting
+// console.log(gameName.indexOf('c')); // know the index of the string to the specific value from the string
+
+
+
+
