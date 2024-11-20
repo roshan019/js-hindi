64539dcf9abfcbd956ffbb7aa28f03defbe8c6ab
roshan019 <139036238+roshan019@users.noreply.github.com>
2024-11-20 18:07:07 +0000

    date and time learining

---
+++ 01-basics/07-date.js
@@ -0,0 +1,40 @@
+// Dates
+
+let myDate = new Date()
+
+// console.log(typeof myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+
+
+// let myCreateDate = new Date(2024,11,20)
+
+// console.log(myCreateDate.toLocaleString());
+
+let myCreateDate = new Date("11-20-2024")
+
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+
+// console.log(myCreateDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000))
+
+let myDates = new Date()
+
+// console.log(myDates);
+// console.log(myDates.getMonth()+1);
+
+// console.log(myDates.getDay());
+
+// myDates.toLocaleString('default',{
+//     weekday: "long"
+// })
+
+console.log(myDates);
