61659839d55496fa5a1315c222233e78eeacb91e
roshan019 <139036238+roshan019@users.noreply.github.com>
2024-11-21 17:13:10 +0000

    learing about the array

---
+++ 02-basics/01-array.js
@@ -0,0 +1,54 @@
+// array
+
+const myArr = [1,4,6, 8,9,5]
+
+// const myHeros = ['shaktiman', 'naagraj']
+
+// const myArr2 = new Array(1,3,4,5,6)
+// console.log(myArr[0]);
+
+// console.log(myArr2);
+
+// console.log(typeof myArr2);
+// console.log(typeof myArr);
+
+
+// Array Methods
+
+// myArr.push(6)
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(4));
+
+
+// const newArr = myArr.join()
+
+// console.log(newArr);
+// console.log(typeof newArr);   //string types
+
+// console.log(myArr);
+
+
+// slice, splice
+// slice doesn't manuplate (it will return the original string as it is) the original string but splice manuplate the string.
+
+// console.log("A", myArr);
+
+// const myn1 = myArr.slice(1,3)
+
+// console.log(myn1);
+// console.log("B", myArr);
+
+// const myn2 = myArr.splice(1,3)  // splice method manuplate the original string. It remove the slice from the string from the original string give a remaining string from that string.
+
+// console.log("c", myArr);   // this will give the remaining string after splice the string
+
+// console.log(myn2);  // give the slice part of the string
+
+
+
+
+
+
+
+
