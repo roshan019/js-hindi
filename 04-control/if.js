4a1abbba0956fa211e8bfe65c89152da9d33c3cd
roshan019 <139036238+roshan019@users.noreply.github.com>
2024-12-01 16:53:06 +0000

    control flow

---
+++ 04-control/if.js
@@ -0,0 +1,36 @@
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+
+// short hand notation
+
+const balance = 1000
+
+// if(balance <500) console.log("test");
+
+// if(balance < 500){
+//     console.log("less than");
+// }else if(balance <750){
+//     console.log("less tham 750");
+    
+// }else{
+//     console.log("less then 1200");
+    
+// }
+
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInfromGoogle = false
+// const loggedInfromEmail = true
+// if(userLoggedIn && debitCard){
+//     console.log("allow to by course");   
+// }
+
+// if(loggedInfromGoogle || loggedInfromEmail){
+//     console.log("user logged in");
+    
+// }
---
+++ 04-control/truthy.js
@@ -0,0 +1,67 @@
+const userEmail = ""
+
+// if(userEmail){
+//     console.log("Got user email");
+    
+// }else{
+//     console.log("don't have user email");
+    
+// }
+
+
+// if(userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("object is empty");
+    
+// }
+
+/************************************************** */
+// falsy values
+
+/*
+false
+0
+-0,
+BigInt
+0n,
+null,
+undefined,
+NaN
+*/
+
+/********************************* */
+//truthy value
+
+/*
+"0",
+"false",
+[],
+" ",
+{},
+function(){}
+*/
+
+/*********************************************** */
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = null ?? 10
+
+// val1 = undefined ?? 15;
+// val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+// terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80"): console.log("more than 80");
+;
