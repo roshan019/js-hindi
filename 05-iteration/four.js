f32908e0d311a7b4aa3dda6e5cfb825d81d19bd8
roshan019 <139036238+roshan019@users.noreply.github.com>
2024-12-02 16:42:44 +0000

    loop in  array, object and function

---
+++ 05-iteration/four.js
@@ -0,0 +1,17 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    py: 'python'
+}
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcup is for ${myObject[key]}`);
+    
+// }
+
+const programming = ['js', 'py', 'rb', 'c++']
+
+for (const key in programming){
+    console.log(programming[key]);
+}
---
+++ 05-iteration/three.js
@@ -0,0 +1,26 @@
+// for of
+
+// const arr = [1, 2, 3, 4, 5]
+// for (const num of arr) {
+//     console.log(num)
+// }
+
+
+// const gretting = "hello world"
+// for (const val of gretting) {
+//     console.log(`each char is ${val}`);
+    
+// }
+
+// maps
+
+// const map = new Map()
+// map.set('IN','india')
+// map.set('fr', 'france')
+// // console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key);
+    
+    
+// }
