2d4d1581662b3b21447191a0635a8b592b1bf262
roshan019 <139036238+roshan019@users.noreply.github.com>
2024-12-03 17:44:32 +0000

    filter map and reduce method

---
+++ 05-iteration/eight.js
@@ -0,0 +1,33 @@
+// const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce( function (acc, currval) {
+//     console.log(`acc: ${acc} and currval ${currval}`);
+    
+//     return acc + currval
+// }, 0)
+// const myTotal = myNums.reduce( (acc, currval) => acc + currval, 0)
+
+// console.log(myTotal);
+
+// const shoppingCart = [
+//     {
+//         itemName: "js course",
+//         price: 2999
+//     },
+//     {
+//         itemName: "python",
+//         price: 2299
+//     },
+//     {
+//         itemName: "mobile dev course",
+//         price: 1299
+//     },
+//     {
+//         itemName: "data science",
+//         price: 12999
+//     }
+// ]
+
+// const priceTopay = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+
+// console.log(priceTopay);
